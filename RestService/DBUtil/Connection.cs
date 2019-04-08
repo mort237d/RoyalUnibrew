5e270deba7d46181707defa3e8f6119e126e4fa5
Morten <morto1995@hotmail.com>
2019-04-08 10:27:52 +0200

    connectionstring updated

--- RestService/DBUtil/Connection.cs
+++ RestService/DBUtil/Connection.cs
@@ -4,7 +4,7 @@ namespace RestService.DBUtil
 {
     public class Connection
     {
-        private const string ConnectionString = @"Data Source=mort237d.database.windows.net;Initial Catalog=RoyalUnibrewDB;User ID=KaffeOgKage;Password=Mort237d;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private const string ConnectionString = @"Data Source=mort237d.database.windows.net;Initial Catalog=UniBase;User ID=KaffeOgKage;Password=Mort237d;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public readonly SqlConnection MyConnection = new SqlConnection(ConnectionString);
         //Static??
     }
