a0e1baaf16b8a3111509121b0e6520cbe9fd8866
LucasvanNiekerk <luca1921@edu.easj.dk>
2019-04-08 12:53:04 +0200

    Kind of works

--- UniBase/App.xaml.cs
+++ UniBase/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using UniBase.View;
 
 namespace UniBase
 {
@@ -66,7 +67,8 @@ namespace UniBase
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    //Todo change this bla
+                    rootFrame.Navigate(typeof(WorkPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
--- UniBase/ViewModel/WorkViewModel.cs
+++ UniBase/ViewModel/WorkViewModel.cs
@@ -1,6 +1,54 @@
-﻿namespace UniBase.ViewModel
+﻿using System.ComponentModel;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ModelLibrary;
+using UniBase.Annotations;
+
+namespace UniBase.ViewModel
 {
-    class WorkViewModel
+    class WorkViewModel : INotifyPropertyChanged
     {
+        private string test= "hej";
+        private const string URI = "http://localhost:12736/api/Product";
+        private HttpClient client = new HttpClient();
+
+
+        public WorkViewModel()
+        {
+            Product v = GetOneFacility(1);
+
+            test = v.ProductName;
+        }
+
+
+        private Product GetOneFacility(int id)
+        {
+            Task<string> resTask = client.GetStringAsync(URI + "/" + id);
+
+            return JsonConvert.DeserializeObject<Product>(resTask.Result);
+        }
+
+        public string Test
+        {
+            get { return test; }
+            set
+            {
+                test = value; 
+                OnPropertyChanged();
+            }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
