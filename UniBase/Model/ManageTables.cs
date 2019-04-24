1c86bb4ba3c2e07be1e82faedf7f0374accb1d79
LucasvanNiekerk <luca1921@edu.easj.dk>
2019-04-24 13:15:59 +0200

    .

---
+++ UniBase/Model/ManageTables.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModelLibrary;
+
+namespace UniBase.Model
+{
+    public class ManageTables
+    {
+
+        private List<Frontpages> _frontpagesList;
+        private List<Products> _productsList;
+        private List<Frontpages> _allFrontpages;
+        public ManageTables()
+        {
+            _allFrontpages = ModelGenerics.GetAll(new Frontpages());
+        }
+
+        private static ManageTables _instance;
+        public static ManageTables Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new ManageTables();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
--- UniBase/Model/ModelGenerics.cs
+++ UniBase/Model/ModelGenerics.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace UniBase
+namespace UniBase.Model
 {
     public static class ModelGenerics
     {
