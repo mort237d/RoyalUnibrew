8df84fd79331d432e82f5981e48f162c4f75b646
Morten <morto1995@hotmail.com>
2019-04-30 13:36:05 +0200

    color styling

---
+++ UniBase/View/Colors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniBase.View
+{
+    public class Colors
+    {
+        public Colors()
+        {
+            RoyalUniBrewOriginalGreen = "#16503F";
+            RoyalUniBrewLightGreen = "#1A634D";
+            RoyalUniBrewLighterGreen = "#238265";
+            RoyalUniBrewAlmostLigthestGreen = "#279171";
+            RoyalUniBrewLightestGreen = "#32BA91";
+        }
+
+        public string RoyalUniBrewOriginalGreen { get; set; }
+        public string RoyalUniBrewLightGreen { get; set; }
+        public string RoyalUniBrewLighterGreen { get; set; }
+        public string RoyalUniBrewAlmostLigthestGreen { get; set; }
+        public string RoyalUniBrewLightestGreen { get; set; }
+
+        
+    }
+}
