2997856e6c36e4cad7437f1df5234c9abb50ae67
Morten <morto1995@hotmail.com>
2019-05-10 13:08:16 +0200

    Buttonmethods classes

---
+++ UniBase/Model/K2/InputValidator.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniBase.Model.K2
+{
+    class InputValidator
+    {
+        /// <summary>
+        /// Rækkefølge på properties i klasser er vigtige!
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="type"></param>
+        public void CheckIfInputsAreValid<T>(ref T type)
+        {
+            List<string> datesandtimespans = new List<string>();
+            int listIndexCounter = 0;
+
+            Type tModelType = type.GetType();
+
+            PropertyInfo[] arrayPropertyInfos = tModelType.GetProperties();
+
+            //ToDo Finish This.
+            foreach (PropertyInfo property in arrayPropertyInfos)
+            {
+                var prop = type.GetType().GetProperty(property.Name, BindingFlags.Public | BindingFlags.Instance);
+                var proppi = prop.GetValue(type);
+
+                if (property.PropertyType == typeof(string))
+                {
+                    if (prop.Name.Contains("StringHelper"))
+                    {
+                        if (proppi.ToString().Length >= 8)
+                        {
+                            datesandtimespans.Add(proppi.ToString());
+                        }
+                        else
+                        {
+                            //error
+                            Debug.WriteLine("Failed");
+                        }
+                    }
+                    else if (proppi == null)
+                    {
+                        if (property.Name == "Note" || property.Name == "CommentsOnChangedDate")
+                        {
+                            prop.SetValue(type, " ", null);
+                        }
+                        else
+                        {
+                            //error
+                            Debug.WriteLine("Failed");
+
+                        }
+                    }
+                }
+                else if (property.PropertyType == typeof(int))
+                {
+                    int.TryParse(proppi.ToString(), out int i);
+                    if (i == 0)
+                    {
+                        //error
+                        Debug.WriteLine("Failed");
+
+                    }
+                }
+                else if (property.PropertyType == typeof(double))
+                {
+                    double.TryParse(proppi.ToString(), out double i);
+                    if (i == 0)
+                    {
+                        //error
+                        Debug.WriteLine("Failed");
+
+                    }
+                }
+                else if (property.PropertyType == typeof(DateTime))
+                {
+                    if (!(datesandtimespans.Count <= listIndexCounter))
+                    {
+
+                        DateTime dt = DateTime.Now;
+                        var split = datesandtimespans[listIndexCounter].Split('/');
+                        var splitWithoutSpecialChars = split;
+                        for (int i = 0; i < split.Length; i++)
+                        {
+                            splitWithoutSpecialChars[i] = split[i].Trim('/');
+                        }
+
+                        if (splitWithoutSpecialChars[2].Length > 4)
+                        {
+                            splitWithoutSpecialChars[2] = splitWithoutSpecialChars[2].Remove(4);
+                        }
+
+                        try
+                        {
+                            prop.SetValue(type, new DateTime(int.Parse(splitWithoutSpecialChars[0]), int.Parse(splitWithoutSpecialChars[1]), int.Parse(splitWithoutSpecialChars[2]), dt.Hour, dt.Minute, 0), null);
+                        }
+                        catch
+                        {
+                            //error
+                        }
+                        listIndexCounter++;
+                    }
+                }
+                else if (property.PropertyType == typeof(TimeSpan))
+                {
+                    if (!(datesandtimespans.Count <= listIndexCounter))
+                    {
+                        var split = datesandtimespans[listIndexCounter].Split(':');
+                        try
+                        {
+                            prop.SetValue(type, new TimeSpan(int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2])), null);
+                        }
+                        catch
+                        {
+                            //error
+                        }
+                        listIndexCounter++;
+                    }
+                }
+            }
+        }
+    }
+}
--- UniBase/ViewModel/WorkViewModel.cs
+++ UniBase/ViewModel/WorkViewModel.cs
@@ -52,9 +52,7 @@ namespace UniBase.ViewModel
 
         public RelayCommand AddUserCommand { get; set; }
         public RelayCommand DeleteUserCommand { get; set; }
-
-        public ManageTables Column_2 { get; set; }
-        public Colors PredefinedColors { get; set; }
+        
         public ManageTables Column_2 { get; set; }
         public PredefinedColors PredefinedColors { get; set; }
         public RelayCommand<object> ControlledClickCommand { get; set; }
@@ -114,16 +112,16 @@ namespace UniBase.ViewModel
 
             #endregion
 
-            RefreshFrontpageTable = new RelayCommand(Column_2.RefreshFrontpages);
-            RefreshLastTenFrontpageTable = new RelayCommand(Column_2.RefreshLastTenFrontpages);
-            SaveFrontpageTable = new RelayCommand(Column_2.SaveFrontpages);
-            AddFrontpageTable = new RelayCommand(Column_2.AddNewFrontpages);
-            DeleteFrontpageTable = new RelayCommand<object>(Column_2.DeleteFrontpage);
+            RefreshFrontpageTable = new RelayCommand(Column_2.FrontpageButtonMethod.RefreshFrontpages);
+            RefreshLastTenFrontpageTable = new RelayCommand(Column_2.FrontpageButtonMethod.RefreshLastTenFrontpages);
+            SaveFrontpageTable = new RelayCommand(Column_2.FrontpageButtonMethod.SaveFrontpages);
+            AddFrontpageTable = new RelayCommand(Column_2.FrontpageButtonMethod.AddNewFrontpages);
+            DeleteFrontpageTable = new RelayCommand<object>(Column_2.FrontpageButtonMethod.DeleteFrontpage);
 
             RefreshControlRegistrationTable = new RelayCommand(Column_2.RefreshControlRegistrations);
             RefreshLastTenControlRegistrationTable = new RelayCommand(Column_2.RefreshLastTenControlRegistrations);
             SaveControlRegistrationTable = new RelayCommand(Column_2.SaveControlRegistrations);
-            AddControlRegistrationsTable = new RelayCommand(Column_2.AddNewControlRegistrations);
+            //AddControlRegistrationsTable = new RelayCommand(Column_2.AddNewControlRegistrations);
 
             RefreshControlScheduleTable = new RelayCommand(Column_2.RefreshControlSchedules);
             RefreshLastTenControlScheduleTable = new RelayCommand(Column_2.RefreshLastTenControlSchedules);
