4e4fe065848756690df621d84ff30b376cbb4e04
Morten <morto1995@hotmail.com>
2019-05-14 13:17:54 +0200

    ...

--- UniBase/Model/TrendAdminstrator.cs
+++ UniBase/Model/TrendAdminstrator.cs
@@ -65,100 +65,100 @@ namespace UniBase.Model
         public void GraphComboboxSelectedMethod(string comboboxInput, string comboboxTimeInput)
         {
             
-                CreateGraph(comboboxInput, comboboxTimeInput);
+                //CreateGraph(comboboxInput, comboboxTimeInput);
             
         }
-        public void CreateGraph(string comboboxInput,string timePeriod)
-        {
-            TrendList.Clear();
-            DateTime tempDayOfScheduleList = CompleteControlSchedulesList[0].Time;
-            int timeHorizon = 0;
-            int timeHorizonDivider = 0;
-            DateTime currentItemDate = DateTime.Now;
-            ConstantValues constantValues = new ConstantValues();
-            double minValue = 0;
-            double maxValue = 0;
+        //public void CreateGraph(string comboboxInput,string timePeriod)
+        //{
+        //    TrendList.Clear();
+        //    DateTime tempDayOfScheduleList = CompleteControlSchedulesList[0].Time;
+        //    int timeHorizon = 0;
+        //    int timeHorizonDivider = 0;
+        //    DateTime currentItemDate = DateTime.Now;
+        //    ConstantValues constantValues = new ConstantValues();
+        //    double minValue = 0;
+        //    double maxValue = 0;
 
-            if (timePeriod == "En Uge")
-            {
-                timeHorizon = 7;
-                timeHorizonDivider = 1;
-            }
-            else if (timePeriod == "En Måned")
-            {
-                timeHorizon = 30;
-                timeHorizonDivider = 3;
-            }
-            else if (timePeriod == "Et Kvartal")
-            {
-                timeHorizon = 91;
-                timeHorizonDivider = 10;
-            }
-            else if (timePeriod == "Et År")
-            {
-                timeHorizon = 365;
-                timeHorizonDivider = 30;
-            }
+        //    if (timePeriod == "En Uge")
+        //    {
+        //        timeHorizon = 7;
+        //        timeHorizonDivider = 1;
+        //    }
+        //    else if (timePeriod == "En Måned")
+        //    {
+        //        timeHorizon = 30;
+        //        timeHorizonDivider = 3;
+        //    }
+        //    else if (timePeriod == "Et Kvartal")
+        //    {
+        //        timeHorizon = 91;
+        //        timeHorizonDivider = 10;
+        //    }
+        //    else if (timePeriod == "Et År")
+        //    {
+        //        timeHorizon = 365;
+        //        timeHorizonDivider = 30;
+        //    }
 
-            int amountOfItemsWithSameDate = 0;
-            double tempTotalValue = 0;
+        //    int amountOfItemsWithSameDate = 0;
+        //    double tempTotalValue = 0;
 
-            for (int i = 0; i < CompleteControlSchedulesList.Count; i++)
-            {
-                if (CompleteControlSchedulesList[i].Time >= DateTime.Now - new TimeSpan(timeHorizon, 0, 0, 0) && CompleteControlSchedulesList[i].Time <= DateTime.Now)
-                {
-                    currentItemDate = CompleteControlSchedulesList[i].Time.Subtract(new TimeSpan(0,
-                        CompleteControlSchedulesList[i].Time.Hour, CompleteControlSchedulesList[i].Time.Minute,
-                        CompleteControlSchedulesList[i].Time.Second));
+        //    for (int i = 0; i < CompleteControlSchedulesList.Count; i++)
+        //    {
+        //        if (CompleteControlSchedulesList[i].Time >= DateTime.Now - new TimeSpan(timeHorizon, 0, 0, 0) && CompleteControlSchedulesList[i].Time <= DateTime.Now)
+        //        {
+        //            currentItemDate = CompleteControlSchedulesList[i].Time.Subtract(new TimeSpan(0,
+        //                CompleteControlSchedulesList[i].Time.Hour, CompleteControlSchedulesList[i].Time.Minute,
+        //                CompleteControlSchedulesList[i].Time.Second));
 
-                    here:
+        //            here:
 
-                    if (tempDayOfScheduleList <= currentItemDate + new TimeSpan(timeHorizonDivider, 0, 0, 0) && tempDayOfScheduleList >= currentItemDate)
-                    {
-                        amountOfItemsWithSameDate++;
-                        if (comboboxInput == "Vægt")
-                        { //todo check om weight er null selvom den ikke burde være
-                            tempTotalValue += (double)CompleteControlSchedulesList[i].Weight;
-                            minValue = constantValues.MinWeight;
-                            maxValue = constantValues.MaxWeight;
+        //            if (tempDayOfScheduleList <= currentItemDate + new TimeSpan(timeHorizonDivider, 0, 0, 0) && tempDayOfScheduleList >= currentItemDate)
+        //            {
+        //                amountOfItemsWithSameDate++;
+        //                if (comboboxInput == "Vægt")
+        //                { //todo check om weight er null selvom den ikke burde være
+        //                    tempTotalValue += (double)CompleteControlSchedulesList[i].Weight;
+        //                    minValue = constantValues.MinWeight;
+        //                    maxValue = constantValues.MaxWeight;
                             
-                        }
-                        else if (comboboxInput == "MipMa")
-                        {
-                            tempTotalValue += (double)CompleteControlSchedulesList[i].MipMA;
-                            minValue = constantValues.MinMipMa;
-                            maxValue = constantValues.MaxMipMa;
-                        }
-                        else if (comboboxInput == "Lud Koncentration")
-                        {
-                            tempTotalValue += (double)CompleteControlSchedulesList[i].LudKoncentration;
-                            minValue = constantValues.MinLudkoncentration;
-                            maxValue = constantValues.MaxLudkoncentration;
-                        }
+        //                }
+        //                else if (comboboxInput == "MipMa")
+        //                {
+        //                    tempTotalValue += (double)CompleteControlSchedulesList[i].MipMA;
+        //                    minValue = constantValues.MinMipMa;
+        //                    maxValue = constantValues.MaxMipMa;
+        //                }
+        //                else if (comboboxInput == "Lud Koncentration")
+        //                {
+        //                    tempTotalValue += (double)CompleteControlSchedulesList[i].LudKoncentration;
+        //                    minValue = constantValues.MinLudkoncentration;
+        //                    maxValue = constantValues.MaxLudkoncentration;
+        //                }
 
-                        continue;
-                    }
+        //                continue;
+        //            }
 
-                    if (amountOfItemsWithSameDate != 0)
-                    {
-                        TrendList.Add(new Trends(tempTotalValue / amountOfItemsWithSameDate, currentItemDate.Year + "/" + currentItemDate.Month + "/" + tempDayOfScheduleList.Day, minValue, maxValue));
-                    }
+        //            if (amountOfItemsWithSameDate != 0)
+        //            {
+        //                TrendList.Add(new Trends(tempTotalValue / amountOfItemsWithSameDate, currentItemDate.Year + "/" + currentItemDate.Month + "/" + tempDayOfScheduleList.Day, minValue, maxValue));
+        //            }
 
-                    tempTotalValue = 0;
-                    amountOfItemsWithSameDate = 0;
+        //            tempTotalValue = 0;
+        //            amountOfItemsWithSameDate = 0;
 
-                    if (new TimeSpan(timeHorizonDivider,0,0,0) <= currentItemDate - tempDayOfScheduleList)
-                    {
-                        tempDayOfScheduleList = currentItemDate;
-                        goto here;
-                    }
-                }
-            }
-            if (amountOfItemsWithSameDate != 0)
-            {
-                TrendList.Add(new Trends(tempTotalValue / amountOfItemsWithSameDate, currentItemDate.Year + "/" + currentItemDate.Month + "/" + tempDayOfScheduleList.Day, minValue, maxValue));
-            }
-        }
+        //            if (new TimeSpan(timeHorizonDivider,0,0,0) <= currentItemDate - tempDayOfScheduleList)
+        //            {
+        //                tempDayOfScheduleList = currentItemDate;
+        //                goto here;
+        //            }
+        //        }
+        //    }
+        //    if (amountOfItemsWithSameDate != 0)
+        //    {
+        //        TrendList.Add(new Trends(tempTotalValue / amountOfItemsWithSameDate, currentItemDate.Year + "/" + currentItemDate.Month + "/" + tempDayOfScheduleList.Day, minValue, maxValue));
+        //    }
+        //}
 
         public event PropertyChangedEventHandler PropertyChanged;
 
