15fb48322b8892963e6f093172445e3dfeea935f
jaco2716 <jacob.welin@hotmail.com>
2019-05-16 11:58:21 +0200

    Calculate

--- UniBase/Model/K2/ButtonMethods/CalculateTUTotal.cs
+++ UniBase/Model/K2/ButtonMethods/CalculateTUTotal.cs
@@ -7,5 +7,19 @@
             int result = (dayEnd / 10) - (dayStart / 10) + 1;
             return result;
         }
+
+        public int CalculatePalletCounter(int firstDayTotal, int secoundDayTotal, int thirdDayTotal, int palletsOn0001)
+        {
+            int result = firstDayTotal + secoundDayTotal + thirdDayTotal + palletsOn0001;
+            return result;
+        }
+
+        public int CalculateTotalKegs(int totalKegsPrPallet, int palletCounter, int pallets)
+        {
+            int result = (totalKegsPrPallet * palletCounter) + pallets;
+            return result;
+        }
+
+
     }
 }
