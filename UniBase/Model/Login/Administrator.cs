bdb0958f2fa4aae2311e7b777410c262e28e9a8e
danielegtved <43374944+danielegtved@users.noreply.github.com>
2019-05-20 11:19:52 +0200

    Users

--- UniBase/Model/Login/Administrator.cs
+++ UniBase/Model/Login/Administrator.cs
@@ -1,6 +1,6 @@
 ﻿namespace UniBase.Model.Login
 {
-    class Administrator : User
+    class Administrator : Users
     {
         public Administrator(string name, string email, string telephoneNumber, string password, string imageSource) : base(name, email, telephoneNumber, password, imageSource)
         {
