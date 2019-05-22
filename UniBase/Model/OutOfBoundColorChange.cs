c7f1ff49ae54222cba61c1672b16c2235cbb113d
jaco2716 <jacob.welin@hotmail.com>
2019-05-22 12:22:03 +0200

    summariiies

--- UniBase/Model/OutOfBoundColorChange.cs
+++ UniBase/Model/OutOfBoundColorChange.cs
@@ -6,7 +6,14 @@ namespace UniBase.Model
     public static class OutOfBoundColorChange
     {
         private static SolidColorBrush colorBrush;
-        //Changes the color of the textboxes that are below the minimum or above the maximum.
+        
+        /// <summary>
+        /// Changes the color of the textboxes that are below the minimum or above the maximum.
+        /// </summary>
+        /// <param name="textValue"></param>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
         public static SolidColorBrush ChangeListViewColor(double textValue, double minValue, double maxValue)
         {
             //If the textbox value is out of the range, change color to lightSalmon
