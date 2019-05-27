cc2576cc5d09d16e2c6f943970ccbbe6fbb2aaef
Morten <morto1995@hotmail.com>
2019-05-27 13:42:20 +0200

    images

--- RestServiceTests/Controllers/ControlRegistrationsControllerTests.cs
+++ RestServiceTests/Controllers/ControlRegistrationsControllerTests.cs
@@ -1,5 +1,9 @@
-﻿using RestService.Controllers;
+﻿using System.Net;
+using System.Threading;
+using System.Web.Http.Results;
+using RestService.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestService.Models;
 
 namespace RestService.Controllers.Tests
 {
@@ -12,5 +16,17 @@ namespace RestService.Controllers.Tests
         {
             Assert.IsNotNull(_controlRegistrationsController.GetControlRegistrations());
         }
+
+        [TestMethod()]
+        public void GetControlRegistrationTest()
+        {
+            //var controlRegistration = _controlRegistrationsController.GetControlRegistration(1);
+            //Assert.IsInstanceOfType(controlRegistration, typeof(OkResult));
+
+            //var controlRegistration = _controlRegistrationsController.GetControlRegistration(10000);
+            //Assert.IsInstanceOfType(controlRegistration, typeof(NotFoundResult));
+        }
+
+        //https://stackoverflow.com/questions/19936892/how-do-i-unit-test-web-api-action-method-when-it-returns-ihttpactionresult
     }
 }
