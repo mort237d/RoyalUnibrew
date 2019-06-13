11df0cf861179c61f963fe3a2f8819be78e979c6
Morten <morto1995@hotmail.com>
2019-06-13 09:22:29 +0200

    Abstarct try out

---
+++ UniBase/Model/K2/TableMethods/AbstractManageTableMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniBase.Model.K2.TableMethods
+{
+    public abstract class AbstractManageTableMethods
+    {
+        public abstract void RefreshAll();
+        public abstract void RefreshLastTen();
+
+        public virtual void SaveAll(int i)
+        {
+
+        }
+        public abstract void DeleteItem();
+        public abstract void AddNewItem();
+        public abstract void SelectParentItem(object obj);
+        public abstract void SortButtonClick(object id);
+    }
+}
